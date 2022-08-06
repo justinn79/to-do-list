176e1d36c75a92204359fa3df6119c2fc1f9bf7e
justinn79 <justinlylemiraflor@gmail.com>
2022-08-06 06:05:33 -0400

    removed extra files

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Blank
-        </p>
-      </header>
+      <p>
+        Blank
+      </p>
     </div>
   );
 }
