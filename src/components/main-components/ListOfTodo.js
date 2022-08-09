f162f9c2a0b187a983eafc23242b9cd5378c0c91
justinn79 <justinlylemiraflor@gmail.com>
2022-08-09 16:45:06 -0400

    implemented the list of todos with hardcoded data

--- src/components/main-components/ListOfTodo.js
+++ src/components/main-components/ListOfTodo.js
@@ -1,10 +1,58 @@
 import React from 'react'
+import Todo from './Todo'
 
 function ListOfTodo(){
 
+    const selectedFolder ="work-related"
+
+    const todos = [
+        {
+            id: '1',
+            text: 'finish todo list project',
+            date: '08/17/2022',
+            time: '10:00 PM',
+            day: '6',
+            checked: false,
+            folder: 'work-related'
+        },
+
+        {
+            id: '2',
+            text: 'watch programming videos',
+            date: '08/14/2022',
+            time: '8:00 PM',
+            day: '3',
+            checked: false,
+            folder: 'fun things'
+        },
+
+        {
+            id: '3',
+            text: 'do laundry',
+            date: '08/11/2022',
+            time: '11:00 AM',
+            day: '1',
+            checked: false,
+            folder: 'chores'
+        },
+    ]
+
     return(
         <div className="ListOfTodo">
-            <h2>ListOfTodo here</h2>
+
+            <div className="selected-Folder">
+                {selectedFolder}
+            </div>
+            
+
+            <div className="todos">
+                {
+                    todos.map( todo =>
+                        <Todo todo={todo} key={todo.id} />)
+                }
+            </div>
+
+
         </div>
     )
 }
