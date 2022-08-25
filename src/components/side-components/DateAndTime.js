bdd2ded2a064c9ed2133e78f33f5226c185319d8
justinn79 <justinlylemiraflor@gmail.com>
2022-08-25 18:17:24 -0400

    fixed past due date filter bug and added new date/time display

--- src/components/side-components/DateAndTime.js
+++ src/components/side-components/DateAndTime.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import moment from 'moment'
 import nightsky from '../../images/night-sky.jpg'
+import { useDate } from '../../hooks'
 
 function DateAndTime(){
+
+    const {date, time, wish} = useDate()
 
     return(
         <div className="DateAndTime">
             <div className="text">
-                <p className="date">{moment().format('dddd, MMMM Do YYYY')}</p>
-                <p className="time">{moment().format('h:mm a')}</p>
+                <p>{wish}</p>
+                <p>{date}</p>
+                <p>{time}</p>
+                {/* <p className="date">{moment().format('dddd, MMMM Do YYYY')}</p>
+                <p className="time">{moment().format('h:mm a')}</p> */}
             </div>
         </div>
     )
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -58,7 +58,20 @@ export function useFilterTodos(todos, selectedFolder){
                 const diffDays = todoDate.diff(todayDate, 'days')
                 const diffTimes = todoTime.diff(todayTime, 'minutes')
 
-                return diffDays <= 0 && diffTimes < 0
+                console.log(diffDays)
+                console.log(diffTimes)
+
+                if(diffDays < 0)
+                {
+                    return true
+                }
+
+                else if(diffDays === 0 && diffTimes < 0)
+                {
+                    return true
+                }
+
+                // return diffDays <= 0 && diffTimes < 0
             })
         }
         else if(selectedFolder === 'completed'){
@@ -73,5 +86,34 @@ export function useFilterTodos(todos, selectedFolder){
 
     return filteredTodos
 }
+
+export const useDate = () => {
+    const locale = 'en';
+    const [today, setDate] = useState(new Date()); // Save the current date to be able to trigger an update
+  
+    useEffect(() => {
+        const timer = setInterval(() => { // Creates an interval which will update the current data every minute
+        // This will trigger a rerender every component that uses the useDate hook.
+        setDate(new Date());
+      }, 60 * 1000);
+      return () => {
+        clearInterval(timer); // Return a funtion to clear the timer so that it will stop being called on unmount
+      }
+    }, []);
+  
+    const day = today.toLocaleDateString(locale, { weekday: 'long' });
+    const date = `${day}, ${today.getDate()} ${today.toLocaleDateString(locale, { month: 'long' })}\n\n`;
+  
+    const hour = today.getHours();
+    const wish = `Good ${(hour < 12 && 'Morning') || (hour < 17 && 'Afternoon') || 'Evening'}, `;
+  
+    const time = today.toLocaleTimeString(locale, { hour: 'numeric', hour12: true, minute: 'numeric' });
+  
+    return {
+      date,
+      time,
+      wish,
+    };
+  };
 
 
